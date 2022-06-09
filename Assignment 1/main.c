690a238299ef04f99e7d48625e6c0caecec852f4
Deltane <71059150+Deltane@users.noreply.github.com>
2022-06-09 18:37:38 +0800

    Delete Assignment 1 directory

--- Assignment 1/main.c
+++
@@ -1,5 +0,0 @@
-//Contains main function
-//Clears terminal screen then prints map with snake and food
-//Contains WASD function which updates the map and refreshes the screen then reprints map
-
-#include <stdio.h>
--- Assignment 1/random.h
+++
@@ -1,7 +0,0 @@
-#ifndef RANDOM_H
-#define RANDOM_H
-
-void initRandom();
-int random(int low, int high);
-
-#endif
